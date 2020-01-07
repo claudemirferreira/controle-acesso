f4e76053f2a1ce74a9c16f2797339764dfb44907
claudemir ramos ferreira <claudemirramosferreira@gmail.com>
2020-01-07 17:05:32 -0400

    incluir o create table

--- controle-acesso-backend/database.sql
+++ controle-acesso-backend/database.sql
@@ -1,42 +1,9 @@
---
--- Table structure for table `tasks`
---
-
-CREATE TABLE `tasks` (
-  `id` int(11) NOT NULL,
-  `task` varchar(200) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '1',
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tasks`
---
-
-INSERT INTO `tasks` (`id`, `task`, `status`, `created_at`) VALUES
-(1, 'Find bugs', 1, '2016-04-10 23:50:40'),
-(2, 'Review code', 1, '2016-04-10 23:50:40'),
-(3, 'Fix bugs', 1, '2016-04-10 23:50:40'),
-(4, 'Refactor Code', 1, '2016-04-10 23:50:40'),
-(5, 'Push to prod', 1, '2016-04-10 23:50:50');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tasks`
---
-ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tasks`
---
-ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
+DROP TABLE IF EXISTS `site`.`user`;
+CREATE TABLE  `site`.`user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(45) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  `autorizado` varchar(1) DEFAULT NULL,
+  `perfil` varchar(1) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
